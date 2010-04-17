069c06da8d99aee708927f5e3846fd6577aaf4ba
Hasan Karahan <hasan.karahan81@gmail.com>
2010-04-17 12:03:50 +0000

    git-svn-id: svn+ssh://blackhan.ch/var/svn/notex@4 aabf4d90-89a0-42e0-9082-2db526a70617

--- head/webed/urls.py
+++ head/webed/urls.py
@@ -1,17 +1,32 @@
 from django.conf.urls.defaults import *
+from django.contrib            import admin
+from views                     import DATA
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^webed/', include('webed.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    (r'^svc/', include('svc.urls', namespace='svc')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+  # url(
+  #     r'^$',
+  #     direct_to_template,
+  #     {'template': 'base.html',
+  #      'extra_context': {}
+  #     },
+  #     name="base"
+  # ),
+
+    url(
+        r'^json/info/$',
+        DATA.info,
+        name='json.info'
+    ),
+
 )
---
+++ head/webed/views.py
@@ -0,0 +1,23 @@
+from django.http                 import HttpResponse
+from django.http                 import Http404
+from django.views.generic.simple import direct_to_template
+from django.template             import TemplateDoesNotExist
+
+import json
+
+class DATA:
+
+    def info (request):
+
+        js_string = json.dumps ({
+            'prj' : 'webed',
+            'ver' : '0.1'
+        })
+
+        return HttpResponse (u'%s\n' % js_string, mimetype='application/json')
+
+    info = staticmethod (info)
+
+if __name__ == "__main__":
+
+    pass
