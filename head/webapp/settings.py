f2189001cca4ab934923c7439699060269d40c2c
Hasan Karahan <hasan.karahan81@gmail.com>
2010-04-17 12:51:55 +0000

    git-svn-id: svn+ssh://blackhan.ch/var/svn/notex@8 aabf4d90-89a0-42e0-9082-2db526a70617

--- head/webapp/settings.py
+++ head/webapp/settings.py
@@ -9,7 +9,7 @@ MANAGERS = ADMINS
 
 import os
 SITE_ROOT = os.path.realpath (os.path.dirname (__file__))
-SITE_NAME = 'webapp' ## notex
+SITE_NAME = 'notex'
 SITE_HOST = 'blackhan.ch'
 
 DATABASE_ENGINE = 'sqlite3'
