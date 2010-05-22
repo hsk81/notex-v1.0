b97285430236dc830ff614e4d9da23d8ce81626e
Hasan Karahan <hasan.karahan81@gmail.com>
2010-05-22 23:17:50 +0000

    minor update
    git-svn-id: svn+ssh://blackhan.ch/var/svn/notex@18 aabf4d90-89a0-42e0-9082-2db526a70617

--- head/notex/editor/admin.py
+++ head/notex/editor/admin.py
@@ -12,6 +12,14 @@ class FILEAdmin (admin.ModelAdmin):
     list_display = ('name', 'type', 'rank', 'project')
     search_fields = ['name', 'type', 'rank', 'project']
 
-admin.site.register (Session)
+class SessionAdmin (admin.ModelAdmin):
+
+    list_display = ('session_key', 'expire_date')
+    list_filter = ['expire_date']
+    date_hierarchy = 'expire_date'
+    search_fields = ['session_key','expire_date']
+
 admin.site.register (PROJECT, PROJECTAdmin)
 admin.site.register (FILE, FILEAdmin)
+admin.site.register (Session, SessionAdmin)
+
