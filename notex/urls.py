fedfaa12ec2d2efcebeee6a3fa0a51e831537b53
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-03 11:46:55 +0500

    minor

--- notex/urls.py
+++ notex/urls.py
@@ -1,22 +1,21 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from views import DATA
+import views
 
 admin.autodiscover ()
 
 urlpatterns = patterns ('',
 
-    (r'^util/',   include('util.urls',   namespace='util')),
-    (r'^svc/',    include('svc.urls',    namespace='svc')),
-    (r'^base/',   include('base.urls',   namespace='base')),
+    (r'^util/', include('util.urls',namespace='util')),
+    (r'^svc/', include('svc.urls', namespace='svc')),
+    (r'^base/', include('base.urls', namespace='base')),
     (r'^editor/', include('editor.urls', namespace='editor')),
 
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include ('django.contrib.admindocs.urls')),
+    (r'^admin/', include (admin.site.urls)),
 
     url (
-        r'^json/info/$', DATA.info, name='json.info'
+        r'^json/info/$', info, name='json.info'
     ),
 
 )
-
--- notex/views.py
+++ notex/views.py
@@ -1,19 +1,11 @@
 from django.http import HttpResponse
 import json
 
-class DATA:
+def info (request):
 
-    def info (request):
+    js_string = json.dumps ({
+    	'prj' : 'notex',
+        'ver' : '0.1'
+    })
 
-        js_string = json.dumps ({
-            'prj' : 'notex',
-            'ver' : '0.1'
-        })
-
-        return HttpResponse (u'%s\n' % js_string, mimetype='application/json')
-
-    info = staticmethod (info)
-
-if __name__ == "__main__":
-
-    pass
+    return HttpResponse (u'%s\n' % js_string, mimetype='application/json')
