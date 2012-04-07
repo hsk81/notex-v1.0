bc56fcdb50313b943e549d39103969fcf96f44ee
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-07 16:44:21 +0500

    OO approach

--- notex/editor/lib/Interpolator.py
+++ notex/editor/lib/Interpolator.py
@@ -13,70 +13,95 @@ from datetime import datetime
 ###############################################################################################
 ###############################################################################################
 
-def clear (key = None):
+class Interpolator:
+
+    def __init__ (self, strict = True):
     
-    if key:
-        _lookup_tbl.pop (key, None)
-    else:
-        _lookup_tbl.clear ()
+        self._predef_tbl = {
+            'date' : lambda x,*xs: x and datetime.today ().strftime (x) or \
+                str (datetime.today ()),
+            'time' : lambda x,*xs: x and datetime.now ().time ().strftime (x) or \
+                str (datetime.now ().time ()),
+        }
 
-def apply (value, key = None, strict = False):
+        self._filter_tbl = {
+            'quote' : lambda value,*args: urllib.quote_plus (value),
+        }
 
-    for head, rest in re.findall ("\${(\w+)\|?(.*?)}", value):
+        self._lookup_tbl = {}
+        self._strict = strict
 
-        if rest != '': el = '${%s|%s}' % (head,rest)
-        else:          el = '${%s}'    %  head
+    def apply (self, value, key = None):
 
-        args = filter (lambda arg: len (arg) > 0, rest.split ('|'))
+        for head, rest in re.findall ("\${(\w+)\|?(.*?)}", value):
 
-        if _lookup_tbl.has_key (head):
-            value = value.replace (el, _modify (*_lookup (head, args), **{'strict':strict}))
-        elif _predef_tbl.has_key (head):
-            value = value.replace (el, _modify (*_predef (head, args), **{'strict':strict}))
-        elif strict:
-            raise UnknowTagException (el)
+            if rest != '':
+                el = '${%s|%s}' % (head,rest)
+            else:
+                el = '${%s}'    %  head
 
-    if key:
-        _lookup_tbl[key] = value
+            args = filter (lambda arg: len (arg) > 0, rest.split ('|'))
 
-    return value
+            if self._lookup_tbl.has_key (head):
+                value = value.replace (el, self._filter (*self._lookup (head, args))
+            elif self._predef_tbl.has_key (head):
+                value = value.replace (el, self._filter (*self._predef (head, args))
+            elif self._strict:
+                raise UnknowTagException (el)
 
-def _lookup (tag, args):
-    return _lookup_tbl[tag], args
+        if key:
+            self._lookup_tbl[key] = value
 
-_lookup_tbl = {}
+        return value
 
-def _predef (tag, args):
-    return _predef_tbl[tag] (*args or [None]), args
+    def clear (self, key = None):
+        
+        if key:
+            self._lookup_tbl.pop (key, None)
+        else:
+            self._lookup_tbl.clear ()
 
-_predef_tbl = {
-    'date' : lambda x,*xs: x and datetime.today ().strftime (x) or \
-        str (datetime.today ()),
-    'time' : lambda x,*xs: x and datetime.now ().time ().strftime (x) or \
-        str (datetime.now ().time ()),
-}
+    def add_predef (self, key, fn):
+        if type (fn) == types.FunctionType:
+            self._predef_tbl[key] = fn
+        else:
+            raise NoFunctionException (fn)
+    def del_predef (self, key):
+        self._predef_tbl.pop (key, None)
 
-def _modify (value, args, strict):
+    def add_filter (self, key, fn):
+        if type (fn) == types.FunctionType:
+            self._filter_tbl[key] = fn
+        else:
+            raise NoFunctionException (fn)
+    def del_filter (self, key):
+        self._filter_tbl.pop (key, None)
 
-    for arg in args:
-        ps = re.findall ("\w+", arg)
-        op = ps.pop (0)
+    def _predef (self, tag, args):
+        return self._predef_tbl[tag] (*args or [None]), args
 
-        if _modify_tbl.has_key (op):
-            value = _modify_tbl[op] (value, ps)
-        elif strict:
-            raise UnknownFilterException (op)
+    def _filter (self, value, args):
+        """
+        TODO: *Whitespace* arguments not possible, e.g. $("lorem ipsum"|swap ' ' '_'), fix!
+        """
 
-    return value
+        for arg in args:
+            ps = re.findall ("\w+", arg)
+            op = ps.pop (0)
 
-_modify_tbl = {
-    'quote' : lambda value,*args: urllib.quote_plus (value)
-}
+            if self._filter_tbl.has_key (op):
+                value = self._filter_tbl[op] (value, ps)
+            elif self._strict:
+                raise UnknownFilterException (op)
 
-## ############################################################################################
+        return value
+
+    def _lookup (self, tag, args):
+        return self._lookup_tbl[tag], args
 
 class UnknownTagException (exceptions.Exception): pass
 class UnknownFilterException (exceptions.Exception): pass
 
 ###############################################################################################
 ###############################################################################################
+
