1d39d030a9e621b13a9e76dde144c0feb6f5d746
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-08 00:31:33 +0500

    #34 Cleaned up and documented interpolator, plus introduced a singleton.

--- notex/editor/lib/Interpolator.py
+++ notex/editor/lib/Interpolator.py
@@ -14,9 +14,48 @@ from datetime import datetime
 ###############################################################################################
 
 class Interpolator:
+    """
+    An interpolator stores associations between a tag like ${tag}, ${tag|arg0|arg1|..|argn},
+    ${tag|filter} or ${tag|filter arg1 arg2 .. 'argn'} with a static or dynamic content. If
+    after the tag a filter is given, then it will be applied to the content. Using these asso -
+    ciations it interpolates string containing interpolation tags.
 
-    def __init__ (self, strict = True):
-    
+    The tag parameters (if any) follow the tag (like filters) after a pipe '|' symbol, whereas
+    filter parameters (if any) are separated by whitespace.
+
+    The pre-defined tags are:
+
+      * ${date}, ${date|format-directive}:
+
+        Are replaced by the current date, and formatted using the given format directive. See
+        http://docs.python.org/library/datetime.html#strftime-strptime-behavior for possible
+        directives.
+
+      * ${time, ${time|format-directive}:
+
+        Are replaced by the current time, and formatted using the given format directive. See
+        http://docs.python.org/library/datetime.html#strftime-strptime-behavior for possible
+        directives.
+
+    The pre-defined filters are:
+
+      * quote:
+
+        Applies url quoting to a string; see http://docs.python.org/library/urllib.html#
+        urllib.quote_plus for a detailed description.
+    """
+
+    def __init__ (self, strict = False):
+        """
+        Initiates a string interpolator.
+
+        Args:
+            strict:
+                Enables interpolator wide strict mode, which means that an interpolation raises
+                either an UnknownTagException, if the interpolated string still contains tags,
+                which have not been defined, or it raises an UnknownFilterException if an
+                undefined filter is used.
+        """
         self._predef_tbl = {
             'date' : lambda x,*xs: x and datetime.today ().strftime (x) or \
                 str (datetime.today ()),
@@ -31,8 +70,31 @@ class Interpolator:
         self._lookup_tbl = {}
         self._strict = strict
 
-    def apply (self, value, key = None):
+    def apply (self, value, key = None, strict = None):
+        """
+        Replaces interpolation tags within a string with their corresponding content and returns
+        it.
 
+        Further for later use, it optionally associates the interpolated string with a tag. In
+        strict mode, if after the interpolation the string still contains some unknown tags,
+        then an exception is raised.
+
+        Args:
+            value: string to be interpolated containing the interpolation tags.
+            key: tag name with which the interpolated string should be associated with.
+            strict: sets strict mode by overwriting the interpolator wide strict mode.
+
+        Returns:
+            Interpolated string with all tags replaced where possible.
+
+        Raises:
+            UnknownTagException:
+                if string contains an unknown tag, but only if strict mode is set, otherwise no
+                exception is raised and tag remains in interpolated string.
+            UnknownFilterException:
+                if string contains an unknown filter, but only if strict mode is set, otherwise
+                no exception is raised.
+        """
         for head, rest in re.findall ("\${(\w+)\|?(.*?)}", value):
 
             if rest != '':
@@ -43,10 +105,14 @@ class Interpolator:
             args = filter (lambda arg: len (arg) > 0, rest.split ('|'))
 
             if self._lookup_tbl.has_key (head):
-                value = value.replace (el, self._filter (*self._lookup (head, args))
+                value = value.replace (el, self._filter (*self._lookup (head, args), \
+                    **{'strict':strict}))
             elif self._predef_tbl.has_key (head):
-                value = value.replace (el, self._filter (*self._predef (head, args))
-            elif self._strict:
+                value = value.replace (el, self._filter (*self._predef (head, args), \
+                    **{'strict':strict}))
+            elif (strict != None) and strict:
+                raise UnknowTagException (el)
+            elif (strict == None) and self._strict:
                 raise UnknowTagException (el)
 
         if key:
@@ -54,54 +120,107 @@ class Interpolator:
 
         return value
 
+    def _lookup (self, tag, args):
+        return self._lookup_tbl[tag], args
+
+    def _predef (self, tag, args):
+        return self._predef_tbl[tag] (*args or [None]), args
+
+    def _filter (self, value, args, strict = None):
+        """
+        TODO: *Whitespace* arguments not possible, e.g. $("lorem ipsum"|swap ' ' '_'), fix!
+        """
+        for arg in args:
+
+            ps = re.findall ("\w+", arg)
+            op = ps.pop (0)
+
+            if self._filter_tbl.has_key (op):
+                value = self._filter_tbl[op] (value, ps)
+            elif (strict != None) and strict:
+                raise UnknowTagException (el)
+            elif (strict == None) and self._strict:
+                raise UnknowTagException (el)
+
+        return value
+
     def clear (self, key = None):
-        
+        """
+        Clears one or all associations between an interpolation tag and the corresponding
+        content(s).
+
+        Args:
+            key: tag to clear; if not set, then all tags will be cleared. If provided tag is
+                 unknown then clear has no effect.
+        """
         if key:
             self._lookup_tbl.pop (key, None)
         else:
             self._lookup_tbl.clear ()
 
     def add_predef (self, key, fn):
+        """
+        Associates a pre-defined dynamic content with a tag. The content is required to be a
+        (lambda) function.
+
+        Args:
+            key: name of the tag the association is created for.
+            fn: function describing the dynamic content.
+
+        Raises:
+            NoFunctionException: if fn should not be of a FunctionType.
+        """
         if type (fn) == types.FunctionType:
             self._predef_tbl[key] = fn
         else:
             raise NoFunctionException (fn)
+
     def del_predef (self, key):
         self._predef_tbl.pop (key, None)
 
     def add_filter (self, key, fn):
+        """
+        Add a filter to the interpolator; it's required to be a (lambda) function.
+
+        Args:
+            key: name of filter.
+            fn: function implementing the filter.
+
+        Raises:
+            NoFunctionException: if fn should not be of a FunctionType.
+        """
         if type (fn) == types.FunctionType:
             self._filter_tbl[key] = fn
         else:
             raise NoFunctionException (fn)
+
     def del_filter (self, key):
         self._filter_tbl.pop (key, None)
 
-    def _predef (self, tag, args):
-        return self._predef_tbl[tag] (*args or [None]), args
-
-    def _filter (self, value, args):
-        """
-        TODO: *Whitespace* arguments not possible, e.g. $("lorem ipsum"|swap ' ' '_'), fix!
-        """
-
-        for arg in args:
-            ps = re.findall ("\w+", arg)
-            op = ps.pop (0)
-
-            if self._filter_tbl.has_key (op):
-                value = self._filter_tbl[op] (value, ps)
-            elif self._strict:
-                raise UnknownFilterException (op)
-
-        return value
-
-    def _lookup (self, tag, args):
-        return self._lookup_tbl[tag], args
+###############################################################################################
+###############################################################################################
 
 class UnknownTagException (exceptions.Exception): pass
 class UnknownFilterException (exceptions.Exception): pass
 class NoFunctionException (exceptions.Exception): pass
+
+###############################################################################################
+###############################################################################################
+
+def apply (value, key = None, strict = None):
+    return _interpolator.apply (value, kye, strict)
+def clear (key = None):
+    return _interpolator.clear (key)
+def add_predef (key, fn):
+    return _interpolator.add_predef (key, fn)
+def del_predef (key):
+    return _interpolator.del_predef (key)
+def add_filter (key, fn):
+    return _interpolator.add_filter (key, fn)
+def del_filter (key):
+    return _interpolator.del_filter (key)
+
+_interpolator = Interpolator ()
 
 ###############################################################################################
 ###############################################################################################
