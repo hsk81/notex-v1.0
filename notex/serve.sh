2d514c35ecd6b94caeee99aa72f25fbcf4ab5837
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-16 14:35:14 +0500

    Close #36: included also 'status'

--- notex/serve.sh
+++ notex/serve.sh
@@ -4,16 +4,41 @@ PROJDIR="$(pwd)"
 PROJECT="$(basename $PROJDIR)"
 
 SRVMETH="threaded"
-HOSTVAL="${1-127.0.0.1}"
-PORTVAL="${2-3001}"
+HOSTVAL="${2-127.0.0.1}"
+PORTVAL="${3-3001}"
 PIDFILE="$PROJDIR/$PROJECT.pid"
 
-cd $PROJDIR
+case "$1" in
+	start)
+		$0 stop
+        exec /usr/bin/env - PYTHONPATH="../python:.." \
+            ./manage.py runfcgi method=$SRVMETH host=$HOSTVAL port=$PORTVAL \
+                pidfile=$PIDFILE
+        ;;
+    stop)
+        if [ -f $PIDFILE ]; then
+            kill $(cat -- $PIDFILE) &> /dev/null
+            rm -f -- $PIDFILE
+        fi
+        ;;
+    restart)
+		$0 start
+        ;;
+    status)
+        if [ -f $PIDFILE ]; then
+            PID=$(cat -- $PIDFILE)
+            STA=$(ps $PID)
+            if [ "$?" -eq "0" ] ; then
+                echo "live: $PID"
+            else
+                echo "dead: $PID"
+            fi
+        else
+            echo "dead"            
+        fi
+        ;;
+    *)
+		$0 start
+esac
 
-if [ -f $PIDFILE ]; then
-    kill $(cat -- $PIDFILE) &> /dev/null
-    rm -f -- $PIDFILE
-fi
-
-exec /usr/bin/env - PYTHONPATH="../python:.." \
-  ./manage.py runfcgi method=$SRVMETH host=$HOSTVAL port=$PORTVAL pidfile=$PIDFILE
+exit 0
