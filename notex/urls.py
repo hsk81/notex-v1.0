1db21f2bc989434e45d860dd7c026e0e8aeb9c05
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-20 15:32:40 +0500

    Close #47: Moved robots.txt to template folder and still dissallowing all bots .. but I guess I've to do some url magic if a want to deceive all bots.

--- notex/urls.py
+++ notex/urls.py
@@ -6,7 +6,6 @@ __date__ = "$Apr 20, 2012 11:35:45 AM$"
 
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.http import HttpResponse
 
 import settings
 import views
@@ -18,14 +17,12 @@ admin.autodiscover ()
 
 urlpatterns = patterns ('',
 
-    (r'^robots\.txt$', lambda r:
-        HttpResponse ("User-agent: *\nDisallow: /", mimetype="text/plain")),
-
     (r'^svc/', include('svc.urls', namespace='svc')),
     (r'^editor/', include('editor.urls', namespace='editor')),
     (r'^admin/doc/', include ('django.contrib.admindocs.urls')),
     (r'^admin/', include (admin.site.urls)),
 
+    (r'^robots\.txt$', views.robots),
 )
 
 if not settings.DEBUG:
--- notex/views.py
+++ notex/views.py
@@ -6,6 +6,7 @@ __date__ = "$Mar 27, 2012 1:12:57 PM$"
 
 from settings import SITE_NAME
 from django.shortcuts import redirect
+from django.views.generic.simple import direct_to_template
 
 ################################################################################
 ################################################################################
@@ -13,6 +14,12 @@ from django.shortcuts import redirect
 def page_not_found (request):
 
     return redirect ('/%s/editor/' % SITE_NAME)
+
+def robots (request):
+
+    return direct_to_template (
+        request, template ='robots.txt', mimetype = 'text/plain'
+    )
 
 ################################################################################
 ################################################################################
