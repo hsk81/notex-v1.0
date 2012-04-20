6235683029f2f06cbaf88414ff958e2acec19b6f
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-20 16:04:30 +0500

    minor

--- notex/manage.py
+++ notex/manage.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python2
+
+################################################################################
+################################################################################
+
+__author__ = "hsk81"
+__date__ = "$Apr 20, 2012 15:59:15 PM$"
+
+################################################################################
+################################################################################
+
 from django.core.management import execute_manager
+
+################################################################################
+################################################################################
+
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write(
-        "Error: Can't find the file 'settings.py' in the directory containing %r. It appears \
-        you've customized things.\nYou'll have to run django-admin.py, passing it your \
-        settings module.\n(If the file settings.py does indeed exist, it's causing an \
-        ImportError somehow.)\n" % __file__)
+        "Error: Can't find the file 'settings.py' in the directory containing \
+        %r. It appears you've customized things.\nYou'll have to run django-\
+        admin.py, passing it your using an ImportError somehow.)\n" % __file__)
     sys.exit(1)
+
+################################################################################
+################################################################################
 
 if __name__ == "__main__":
     execute_manager(settings)
+
+################################################################################
+################################################################################
