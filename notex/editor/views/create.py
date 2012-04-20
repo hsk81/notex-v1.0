4b2195bd5028315b843fffc23a291122a4583b64
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-20 14:12:21 +0500

    Close #50: Had to take max. rank, instead of just next increment

--- notex/editor/views/create.py
+++ notex/editor/views/create.py
@@ -7,6 +7,7 @@ __date__ = "$Mar 10, 2012 1:07:38 AM$"
 from settings import MEDIA_ROOT
 from django.db import transaction
 from django.http import HttpResponse
+from django.db.models import Max
 
 from editor.models import ROOT
 from editor.models import NODE, NODE_TYPE
@@ -23,29 +24,37 @@ import os
 @transaction.commit_on_success
 def createProject (request, path = MEDIA_ROOT + 'app/editor/'):
 
-    (type, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+    (_, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+
+    type = NODE_TYPE.objects.get (_code='project')
     root = ROOT.objects.get (_usid=request.session.session_key)
 
     node = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='project'),
+        type = type,
         root = root,
         name = request.POST['name'],
-        rank = request.POST['rank'])
+        rank = get_next_rank (root))
+
+    type = LEAF_TYPE.objects.get (_code='text')
+    text = open (os.path.join (path,'generic/index.rst')).read ()
 
     _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
+        type = type,
         node = node,
         name = 'index.txt',
-        text = open (os.path.join (path,'generic/index.rst')).read (),
+        text = text,
         rank = 0)
 
+    type = LEAF_TYPE.objects.get (_code='text')
+    text = open (os.path.join (path,'generic/index.yml')).read () \
+        .replace ('${PROJECT}', request.POST['name']) \
+        .replace ('${AUTHORs}', 'AUTHORs')
+
     _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
+        type = type,
         node = node,
         name = 'index.cfg',
-        text = open (os.path.join (path,'generic/index.yml')).read ()
-            .replace ('${PROJECT}', request.POST['name'])
-            .replace ('${AUTHORs}', 'AUTHORs'),
+        text = text,
         rank = 1)
 
     js_string = json.dumps ([{
@@ -57,15 +66,18 @@ def createProject (request, path = MEDIA_ROOT + 'app/editor/'):
 @transaction.commit_on_success
 def createFolder (request):
 
-    (type, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+    (_, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+
+    root = ROOT.objects.get (_usid=request.session.session_key)
+    type = NODE_TYPE.objects.get (_code='folder')
+    node = NODE.objects.get (pk=ids[0])
 
     node = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='folder'),
-        root = ROOT.objects.get (_usid=request.session.session_key),
-        node = NODE.objects.get (pk=ids[0]),
+        type = type,
+        root = root,
+        node = node,
         name = request.POST['name'],
-        rank = request.POST['rank'],
-    )
+        rank = get_next_rank (node))
 
     js_string = json.dumps ([{
         'success' : True,
@@ -76,14 +88,17 @@ def createFolder (request):
 @transaction.commit_on_success
 def createText (request):
 
-    (type, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+    (_, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+
+    type = LEAF_TYPE.objects.get (_code='text')
+    node = NODE.objects.get (pk=ids[0])
 
     leaf = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = NODE.objects.get (pk=ids[0]),
+        type = type,
+        node = node,
         name = request.POST['name'],
         text = request.POST['data'].replace ('\r\n','\n'),
-        rank = int (request.POST['rank']))
+        rank = get_next_rank (node))
 
     if 'leafId' in request.POST:
         js_string = json.dumps ([{
@@ -103,14 +118,17 @@ def createText (request):
 @transaction.commit_on_success
 def createImage (request):
 
-    (type, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+    (_, ids) = json.loads (base64.b32decode (request.POST['nodeId']))
+
+    type = LEAF_TYPE.objects.get (_code='image')
+    node = NODE.objects.get (pk=ids[0])
 
     leaf = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='image'),
-        node = NODE.objects.get (pk=ids[0]),
+        type = type,
+        node = node,
         name = request.POST['name'],
         text = request.POST['data'],
-        rank = int (request.POST['rank']))
+        rank = get_next_rank (node))
 
     if 'leafId' in request.POST:
         js_string = json.dumps ([{
@@ -126,6 +144,35 @@ def createImage (request):
                 json.dumps (('leaf', [leaf.node.pk, leaf.pk])))}])
 
     return HttpResponse (js_string, mimetype='application/json')
+
+################################################################################
+
+def get_next_rank (node):
+
+    leaf_rank = None
+    leafs = LEAF.objects.filter (_node=node)
+    if leafs.count () > 0:
+        aggregate = leafs.aggregate (Max ('_rank'))
+        if aggregate.has_key ('_rank__max'):
+            leaf_rank = aggregate['_rank__max']
+
+    node_rank = None
+    nodes = NODE.objects.filter (_node=node)
+    if nodes.count () > 0:
+        aggregate = nodes.aggregate (Max ('_rank'))
+        if aggregate.has_key ('_rank__max'):
+            node_rank = aggregate['_rank__max']
+
+    if leaf_rank:
+        if node_rank:
+            return 1 + max (leaf_rank, node_rank)
+        else:
+            return 1 + leaf_rank
+    else:
+        if node_rank:
+            return 1 + node_rank
+        else:
+            return 1
 
 ################################################################################
 ################################################################################
