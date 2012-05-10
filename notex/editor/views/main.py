4fd585727a1a9fde7aee0d185c1e5e7898ae5eb6
Hasan Karahan <hasan.karahan81@gmail.com>
2012-05-10 15:07:00 +0500

    #25 Have included (or updated) the simple & complex articles plus the report and the quickstart to default projects

--- notex/editor/views/main.py
+++ notex/editor/views/main.py
@@ -13,125 +13,15 @@ from editor.models import ROOT, ROOT_TYPE
 from editor.models import NODE, NODE_TYPE
 from editor.models import LEAF, LEAF_TYPE
 
+import mimetypes
 import os.path
+import base64
 import sys
 import cgi
 import os
 
 ################################################################################
 ################################################################################
-
-def init_prj01 (root, path):
-
-    prj = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='project'),
-        root = root,
-        name = 'Quickstart',
-        rank = 0
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.txt',
-        text = open (os.path.join (path,'quickstart/index.rst')).read (),
-        rank = 0,
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.cfg',
-        text = open (os.path.join (path,'quickstart/index.yml')).read (),
-        rank = 1,
-    )
-
-def init_prj02 (root, path):
-
-    prj = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='project'),
-        root = root,
-        name = 'Simple Article',
-        rank = 1
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.txt',
-        text = open (os.path.join (path,'simple-article/index.rst')).read (),
-        rank = 0,
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.cfg',
-        text = open (os.path.join (path,'simple-article/index.yml')).read (),
-        rank = 1,
-    )
-
-def init_prj03 (root, path):
-
-    prj = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='project'),
-        root = root,
-        name = 'Complex Article',
-        rank = 2
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.txt',
-        text = open (os.path.join (path,'complex-article/index.rst')).read (),
-        rank = 0,
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.cfg',
-        text = open (os.path.join (path,'complex-article/index.yml')).read (),
-        rank = 1,
-    )
-
-def init_prj04 (root, path):
-
-    prj = NODE.objects.create (
-        type = NODE_TYPE.objects.get (_code='project'),
-        root = root,
-        name = 'Report',
-        rank = 3
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.txt',
-        text = open (os.path.join (path,'report/index.rst')).read (),
-        rank = 0,
-    )
-
-    _ = LEAF.objects.create (
-        type = LEAF_TYPE.objects.get (_code='text'),
-        node = prj,
-        name = 'index.cfg',
-        text = open (os.path.join (path,'report/index.yml')).read (),
-        rank = 1,
-    )
-
-def init (request):
-
-    root = ROOT.objects.create (
-        type = ROOT_TYPE.objects.get (_code='root'),
-        usid = request.session.session_key,
-    )
-
-    init_prj01 (root, MEDIA_ROOT + 'app/editor/')
-    init_prj02 (root, MEDIA_ROOT + 'app/editor/')
-    init_prj03 (root, MEDIA_ROOT + 'app/editor/')
-    init_prj04 (root, MEDIA_ROOT + 'app/editor/')
 
 def main (request):
 
@@ -154,6 +44,247 @@ def main (request):
             'timestamp': request.session['timestamp']
         }
     )
+
+################################################################################
+
+def init (request):
+
+    root = ROOT.objects.create (
+        type = ROOT_TYPE.objects.get (_code='root'),
+        usid = request.session.session_key,
+    )
+
+    media_path = os.path.join (MEDIA_ROOT, 'app', 'editor')
+
+    init_prj01 (root, media_path, prj_rank = 0)
+    init_prj02 (root, media_path, prj_rank = 1)
+    init_prj03 (root, media_path, prj_rank = 2)
+    init_prj04 (root, media_path, prj_rank = 3)
+
+################################################################################
+
+def init_prj01 (root, path, prj_rank = 0):
+
+    prj_name = 'Quickstart'
+    prj_path = 'quickstart'
+
+    prj, rank = NODE.objects.create (
+        type = NODE_TYPE.objects.get (_code='project'),
+        root = root,
+        name = prj_name,
+        rank = prj_rank
+    ), 0
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.cfg',
+        text = get_text_data (path, os.path.join (prj_path,'index.yml')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.txt',
+        text = get_text_data (path, os.path.join (prj_path,'index.rst')),
+        rank = rank,
+    ), rank + 1
+
+def init_prj02 (root, path, prj_rank = 0):
+
+    prj_name = 'Simple Article'
+    prj_path = 'simple-article'
+
+    prj, rank = NODE.objects.create (
+        type = NODE_TYPE.objects.get (_code='project'),
+        root = root,
+        name = prj_name,
+        rank = prj_rank
+    ), 0
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.cfg',
+        text = get_text_data (path, os.path.join (prj_path,'index.yml')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.txt',
+        text = get_text_data (path, os.path.join (prj_path,'index.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'math.txt',
+        text = get_text_data (path, os.path.join (prj_path,'math.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = 'emcc.jpg',
+        text = get_image_data (path, os.path.join (prj_path,'emcc.jpg')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = "tm'49.jpg",
+        text = get_image_data (path, os.path.join (prj_path,"tm'49.jpg")),
+        rank = rank,
+    ), rank + 1
+
+def init_prj03 (root, path, prj_rank = 0):
+
+    prj_name = 'Complex Article'
+    prj_path = 'complex-article'
+
+    prj, rank = NODE.objects.create (
+        type = NODE_TYPE.objects.get (_code='project'),
+        root = root,
+        name = prj_name,
+        rank = prj_rank
+    ), 0
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.cfg',
+        text = get_text_data (path, os.path.join (prj_path,'index.yml')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.txt',
+        text = get_text_data (path, os.path.join (prj_path,'index.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'math.txt',
+        text = get_text_data (path, os.path.join (prj_path,'math.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = 'emcc.jpg',
+        text = get_image_data (path, os.path.join (prj_path,'emcc.jpg')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = "tm'49.jpg",
+        text = get_image_data (path, os.path.join (prj_path,"tm'49.jpg")),
+        rank = rank,
+    ), rank + 1
+
+def init_prj04 (root, path, prj_rank = 0):
+
+    prj_name = 'Report'
+    prj_path = 'report'
+
+    prj, rank = NODE.objects.create (
+        type = NODE_TYPE.objects.get (_code='project'),
+        root = root,
+        name = prj_name,
+        rank = prj_rank
+    ), 0
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.cfg',
+        text = get_text_data (path, os.path.join (prj_path,'index.yml')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank  = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'index.txt',
+        text = get_text_data (path, os.path.join (prj_path,'index.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank  = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'math.txt',
+        text = get_text_data (path, os.path.join (prj_path,'math.rst')),
+        rank = rank,
+    ), rank + 1
+
+    for index in range (9):
+    
+        name = 'chapter-%02d.txt' % index
+        rest = 'chapter-%02d.rst' % index
+
+        _, rank  = LEAF.objects.create (
+            type = LEAF_TYPE.objects.get (_code='text'),
+            node = prj,
+            name = name,
+            text = get_text_data (path, os.path.join (prj_path, rest)),
+            rank = rank,
+        ), rank + 1
+
+    _, rank  = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='text'),
+        node = prj,
+        name = 'footnotes.txt',
+        text = get_text_data (path, os.path.join (prj_path,'footnotes.rst')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = 'emcc.jpg',
+        text = get_image_data (path, os.path.join (prj_path,'emcc.jpg')),
+        rank = rank,
+    ), rank + 1
+
+    _, rank = LEAF.objects.create (
+        type = LEAF_TYPE.objects.get (_code='image'),
+        node = prj,
+        name = "tm'49.jpg",
+        text = get_image_data (path, os.path.join (prj_path,"tm'49.jpg")),
+        rank = rank,
+    ), rank + 1
+
+################################################################################
+
+def get_text_data (path, filename):
+
+    with open (os.path.join (path, filename)) as file:
+
+        return file.read ()
+
+def get_image_data (path, filename):
+
+    with open (os.path.join (path, filename)) as file:
+
+        path, name = os.path.split (filename)
+        mimetype, encoding = mimetypes.guess_type (name)
+
+        return 'data:%s;base64,%s' % \
+            (mimetype, base64.encodestring (file.read ()))
 
 ################################################################################
 ################################################################################
