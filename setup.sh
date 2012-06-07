a3fde76d650f85b94e8ea2530375fdfc6a7b8f5d
Hasan Karahan <hasan.karahan81@gmail.com>
2012-06-07 18:13:45 +0500

    Setup file with virtualenv

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git submodule update --init
+
+virtualenv .
+source bin/activate
+
+pip install django
+pip install pylibmc
+pip install django_pdb
+pip install numpy
+pip install pyyaml
+pip install flup
+
+./manage.py syncdb --noinput
+./serve.sh # lighttpd & memcached!
+
+deactivate
