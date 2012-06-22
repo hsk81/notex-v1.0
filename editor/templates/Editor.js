71bebbf9c3167133a7c4067ea34a9491fdb47f48
Hasan Karahan <hasan.karahan81@gmail.com>
2012-06-22 17:57:36 +0500

    Close #79: Done with even auto-scaling if overlow ;P

--- editor/templates/Editor.js
+++ editor/templates/Editor.js
@@ -1,5 +1,159 @@
 var editor = function () {
 
+    // #########################################################################
+    var tbar_items = [{
+    // #########################################################################
+
+        xtype: 'buttongroup',
+        title: 'Document',
+        columns: 4,
+        defaults: { scale: 'large'},
+        items: [{
+            text : 'Save',
+            iconCls : 'icon-disk-32',
+            iconAlign: 'left',
+            tooltip : '<b>Save</b><br/>Save selected file (to <i>remote</i> storage)',
+            handler : function (button, event) {
+                var editor = Ext.getCmp ('editor.id')
+                var tab = editor.getActiveTab ()
+                if (tab != undefined) {
+                    var tree = Ext.getCmp ('reportManager.tree.id')
+                    var node = tree.getNodeById (tab.id)
+                    var attr = node.attributes
+
+                    if (String (attr['iconCls']).match ("^icon-image$") == "icon-image") {
+                        Ext.getCmp ('reportManager.id').fireEvent ('saveImageTab', tab)
+                    } else {
+                        Ext.getCmp ('reportManager.id').fireEvent ('saveTextTab', tab)
+                    }
+                }
+            }
+        },{
+            text : 'Open',
+            iconCls : 'icon-folder_page-32',
+            iconAlign: 'left',
+            tooltip : '<b>Open</b><br/>Open a text or image file (from <i>local</i> storage)',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('openFile')
+            }
+        }]
+
+    },{
+
+        xtype: 'buttongroup',
+        title: 'Manage',
+        columns: 3,
+        defaults: { scale: 'large'},
+        items: [{
+            text : 'Add',
+            iconCls : 'icon-add-32',
+            xtype :'splitbutton',
+            tooltip : '<b>Add</b><br/>Add a new report, folder or file',
+            menu : {
+                xtype : 'menu',
+                plain : true,
+
+                items : [{
+                    iconCls : 'icon-report',
+                    text : 'Report',
+                    handler : function (button, event) {
+                        Ext.getCmp ('reportManager.id').fireEvent ('addReport')
+                    }
+                },{
+                    iconCls : 'icon-folder',
+                    text : 'Folder',
+                    handler : function (button, event) {
+                        Ext.getCmp ('reportManager.id').fireEvent ('addFolder')
+                    }
+                },{
+                    iconCls : 'icon-page',
+                    text : 'Plain Text',
+                    handler : function (button, event) {
+                        Ext.getCmp ('reportManager.id').fireEvent ('addTextFile')
+                    }
+                }]
+            }
+        },{
+            text : 'Rename',
+            iconCls : 'icon-pencil-32',
+            iconAlign: 'left',
+            tooltip : '<b>Rename</b><br/>Rename selected report, folder or file',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('renameSelectedNode')
+            }
+        },{
+            text : 'Delete',
+            iconCls : 'icon-delete-32',
+            iconAlign: 'left',
+            tooltip : '<b>Delete</b><br/>Delete selected report, folder or file',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('deleteSelectedNode')
+            }
+        }]
+
+    },{
+
+        xtype: 'buttongroup',
+        title: 'Reports',
+        columns: 2,
+        defaults: { scale: 'large'},
+        items: [{
+            text: 'Import',
+            iconCls: 'icon-page_white_zip-32',
+            iconAlign: 'left',
+            tooltip : '<b>Import</b><br/>Open a report from a <b>ZIP</b> archive (at <i>local</i> storage)',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('importReport')
+            }
+        },{
+            text: 'Export',
+            iconCls: 'icon-report_go-32',
+            iconAlign: 'left',
+            tooltip : '<b>Export</b><br/>Save selected report (to <i>local</i> storage)',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('exportReport', urls.exportReport)
+            }
+        }]
+
+    },{
+
+        xtype: 'buttongroup',
+        title: 'Export as ..',
+        columns: 4,
+        defaults: { scale: 'large'},
+        items: [{
+            id : 'btnExportTextId',
+            text : 'Text',
+            iconCls : 'icon-page_white_text-32',
+            iconAlign: 'left',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('exportText')
+            }
+        },{
+            text : 'LaTex',
+            iconCls : 'icon-page_white_code-32',
+            iconAlign: 'left',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('exportLatex')
+            }
+        },{
+            text : 'PDF',
+            iconCls : 'icon-page_white_acrobat-32',
+            iconAlign: 'left',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('exportPdf')
+            }
+        },{
+            text : 'HTML',
+            iconCls : 'icon-page_white_world-32',
+            iconAlign: 'left',
+            handler : function (button, event) {
+                Ext.getCmp ('reportManager.id').fireEvent ('exportHtml')
+            }
+        }]
+
+    }]
+
     function _createTextTab (tabInfo) {
 
         var tab = this.findById (tabInfo.id);
@@ -121,12 +275,37 @@ var editor = function () {
         }
     }
 
+    function _changeIconClassFrom32To16 (toolbar, lastOverflow) {
+        if (lastOverflow) {
+            for (var idx in toolbar.items.items) {
+                var group = toolbar.items.items[idx]
+                if (group.hidden) {
+                    var buttons = group.findByType ('button')
+                    for (var jdx in buttons) {
+                        var button = buttons[jdx]
+                        if (button && button.iconCls) {
+                            button.iconCls = button.iconCls.replace('-32', '')
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     return new Ext.TabPanel ({
 
         activeTab : 0,
         id : 'editor.id',
         enableTabScroll : true,
         tabPosition : 'bottom',
+
+        tbar : {
+            items : tbar_items,
+            enableOverflow: true,
+            listeners: {
+                overflowchange : _changeIconClassFrom32To16
+            }
+        },
 
         listeners : {
             createTextTab : _createTextTab,
--- editor/templates/ReportManager.js
+++ editor/templates/ReportManager.js
@@ -1,16 +1,79 @@
 var reportManager = function () {
 
     // #########################################################################
-    var tbar_items = [
+    var tbar_items = [{
     // #########################################################################
-    {
-        iconCls : 'icon-report_add',
+        iconCls : 'icon-disk',
+        tooltip : '<b>Save</b><br/>Save selected file (to <i>remote</i> storage)',
+        handler : function (button, event) {
+            var editor = Ext.getCmp ('editor.id')
+            var tab = editor.getActiveTab ()
+            if (tab != undefined) {
+                var tree = Ext.getCmp ('reportManager.tree.id')
+                var node = tree.getNodeById (tab.id)
+                var attr = node.attributes
+
+                if (String (attr['iconCls']).match ("^icon-image$") == "icon-image") {
+                    Ext.getCmp ('reportManager.id').fireEvent ('saveImageTab', tab)
+                } else {
+                    Ext.getCmp ('reportManager.id').fireEvent ('saveTextTab', tab)
+                }
+            }
+        }
+    },{
+        iconCls : 'icon-folder_page',
+        tooltip : '<b>Open</b><br/>Open a text or image file (from <i>local</i> storage)',
+        handler : function (button, event) {
+            Ext.getCmp ('reportManager.id').fireEvent ('openFile')
+        }
+    },'-',{
+        iconCls : 'icon-add',
+        tooltip : '<b>Add</b><br/>Add a new report, folder or file',
+        split   : true,
+        menu    : {
+            xtype : 'menu',
+            plain : true,
+
+            items : [{
+                iconCls : 'icon-report',
+                text    : 'Report',
+                handler : function (button, event) {
+                    Ext.getCmp ('reportManager.id').fireEvent ('addReport')
+                }
+            },{
+                iconCls : 'icon-folder',
+                text    : 'Folder',
+                handler : function (button, event) {
+                    Ext.getCmp ('reportManager.id').fireEvent ('addFolder')
+                }
+            },{
+                iconCls : 'icon-page',
+                text    : 'Plain Text',
+                handler : function (button, event) {
+                    Ext.getCmp ('reportManager.id').fireEvent ('addTextFile')
+                }
+            }]
+        }
+    },{
+        iconCls : 'icon-pencil',
+        tooltip : '<b>Rename</b><br/>Rename selected report, folder or file',
+        handler : function (button, event) {
+            Ext.getCmp ('reportManager.id').fireEvent ('renameSelectedNode')
+        }
+    },{
+        iconCls : 'icon-delete',
+        tooltip : '<b>Delete</b><br/>Delete selected report, folder or file',
+        handler : function (button, event) {
+            Ext.getCmp ('reportManager.id').fireEvent ('deleteSelectedNode')
+        }
+    },'-',{
+        iconCls : 'icon-page_white_zip',
         tooltip : '<b>Import</b><br/>Open a report from a <b>ZIP</b> archive (at <i>local</i> storage)',
         handler : function (button, event) {
             Ext.getCmp ('reportManager.id').fireEvent ('importReport')
         }
     },{
-        iconCls : 'icon-page_white_zip',
+        iconCls : 'icon-report_go',
         tooltip : '<b>Export</b><br/>Save selected report (to <i>local</i> storage)',
         split : true,
 
@@ -51,80 +114,15 @@ var reportManager = function () {
           }]
         }
     },'-',{
-        iconCls : 'icon-folder_page',
-        tooltip : '<b>Open</b><br/>Open a text or image file (from <i>local</i> storage)',
-        handler : function (button, event) {
-            Ext.getCmp ('reportManager.id').fireEvent ('openFile')
-        }
-    },{
-        iconCls : 'icon-disk',
-        tooltip : '<b>Save</b><br/>Save selected file (to <i>remote</i> storage)',
-        handler : function (button, event) {
-
-            var editor = Ext.getCmp ('editor.id')
-            var tab = editor.getActiveTab ()
-            if (tab != undefined) {
-                var tree = Ext.getCmp ('reportManager.tree.id')
-                var node = tree.getNodeById (tab.id)
-                var attr = node.attributes
-
-                if (String (attr['iconCls']).match ("^icon-image$") == "icon-image") {
-                    Ext.getCmp ('reportManager.id').fireEvent ('saveImageTab', tab)
-                } else {
-                    Ext.getCmp ('reportManager.id').fireEvent ('saveTextTab', tab)
-                }
-            }
-        }
-    },'-',{
-        iconCls : 'icon-add',
-        tooltip : '<b>Add</b><br/>Add a new report, folder or file',
-        split   : true,
-        menu    : {
-          xtype : 'menu',
-          plain : true,
-
-          items : [{
-              iconCls : 'icon-report',
-              text    : 'Report',
-              handler : function (button, event) {
-                  Ext.getCmp ('reportManager.id').fireEvent ('addReport')
-              }
-          },{
-              iconCls : 'icon-folder',
-              text    : 'Folder',
-              handler : function (button, event) {
-                  Ext.getCmp ('reportManager.id').fireEvent ('addFolder')
-              }
-          },{
-              iconCls : 'icon-page',
-              text    : 'Plain Text',
-              handler : function (button, event) {
-                  Ext.getCmp ('reportManager.id').fireEvent ('addTextFile')
-              }
-          }]
-        }
-    },{
-        iconCls : 'icon-pencil',
-        tooltip : '<b>Rename</b><br/>Rename selected report, folder or file',
-        handler : function (button, event) {
-            Ext.getCmp ('reportManager.id').fireEvent ('renameSelectedNode')
-        }
-    },{
-        iconCls : 'icon-delete',
-        tooltip : '<b>Delete</b><br/>Delete selected report, folder or file',
-        handler : function (button, event) {
-            Ext.getCmp ('reportManager.id').fireEvent ('deleteSelectedNode')
-        }
-    },'-',{
+        id : 'btnMoveUp',
         iconCls : 'icon-arrow_up',
-        id      : 'btnMoveUp',
         tooltip : '<b>Move Up</b><br/>Move selected report, folder or file up in tree',
         handler : function (button, event) {
             Ext.getCmp ('reportManager.id').fireEvent ('moveSelectedNodeUp')
         }
     },{
+        id : 'btnMoveDown',
         iconCls : 'icon-arrow_down',
-        id      : 'btnMoveDown',
         tooltip : '<b>Move Down</b><br/>Move selected report, folder or file down in tree',
         handler : function (button, event) {
             Ext.getCmp ('reportManager.id').fireEvent ('moveSelectedNodeDown')
