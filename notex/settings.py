7f8e07d061fa4b85d379304008bd64313f1eca21
Hasan Karahan <hasan.karahan81@gmail.com>
2012-06-27 12:14:48 +0200

    Clickjack troublesome .. just ignore for now; fix later

--- notex/settings.py
+++ notex/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+ ## 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 if DEBUG:
