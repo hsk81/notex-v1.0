844016bdde5380ed22a2436f35aa61dd77ad1740
Hasan Karahan <hasan.karahan81@gmail.com>
2012-06-28 16:31:23 +0500

    Better file namings

--- serve-as.sh
+++ serve-as.sh
@@ -4,5 +4,5 @@ EXECUSR="${1-http}"
 EXECGRP="${2-http}"
 
 sudo chown $EXECUSR:$EXECGRP . -R
-sudo -u $EXECUSR -g $EXECGRP \
-    ./serve.sh restart
+sudo -u $EXECUSR -g $EXECGRP ./serve.sh \
+    $3 $4 $5 $6 $7 $8
