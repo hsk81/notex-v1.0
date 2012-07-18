d6fb8df6dd6fb8ffef3ee4b0f92e27e1a5157cd0
Hasan Karahan <hasan.karahan81@gmail.com>
2012-07-18 12:56:35 +0500

    Close #101: Upgraded script with multi-instance management capabilities.

--- serve.sh
+++ serve.sh
@@ -1,60 +1,130 @@
 #!/bin/bash
 
-ACTMETH="${1-restart}"
-HOSTVAL="${2-127.0.0.1}"
-PORTVAL="${3-3001}"
-CGIPROT="${4-fcgi}"
-SRVMETH="${5-threaded}"
-DAEMOON="${6-true}"
+###############################################################################
+###############################################################################
 
-PROJDIR="$(pwd)"
-PIDFILE="$PROJDIR/.pid"
+HOSTDEF="127.0.0.1"
+PORTDEF="3001"
+
+###############################################################################
+###############################################################################
+
+function hostval() {
+    local hostval=${1/:*/}
+    if [ -z "${hostval}" ]
+    then
+        echo $2 ## default
+    else
+        echo ${hostval}
+    fi
+}
+
+function portval() {
+    local hostval=${1/:*/}
+    local portval=${1/*:/}
+    if [ -z "${portval}" ]
+    then
+        echo $2 ## default
+    else
+        echo ${portval/$hostval/$2}
+    fi
+}
+
+###############################################################################
+###############################################################################
+
+ACTMETH=${1-restart}
+HOSTVAL=$(hostval ${2-$HOSTDEF:$PORTDEF} $HOSTDEF)
+PORTVAL=$(portval ${2-$HOSTDEF:$PORTDEF} $PORTDEF)
+CGIPROT=${3-fcgi}
+SRVMETH=${4-threaded}
+DAEMOON=${5-true}
+
+PROJDIR=$(pwd)
+PIDFILE=$PROJDIR/.pid-$PORTVAL
+
+###############################################################################
+###############################################################################
+
+function start() {
+    if [ -f $1 ]; then
+        local pid=$(cat -- $1)
+        local sta=$(ps $pid)
+        if [ "$?" -eq "0" ] ; then
+            exit 0
+        fi
+        rm -f -- $1
+    fi
+
+    if [ -f bin/activate ] ; then
+        source bin/activate
+    fi
+
+    exec ./manage.py runfcgi \
+        method=$SRVMETH protocol=$CGIPROT \
+        host=$HOSTVAL port=$PORTVAL \
+        pidfile=$1 daemonize=$DAEMOON
+}
+
+function stop() {
+    if [ -f $1 ]; then
+        kill $(cat -- $1) &> /dev/null
+        rm -f -- $1
+    fi
+}
+
+function status() {
+    if [ -f $1 ]; then
+        PID=$(cat -- $1)
+        STA=$(ps $PID)
+        if [ "$?" -eq "0" ] ; then
+            echo "[$1] live: $PID"
+        else
+            echo "[$1] dead: $PID"
+        fi
+    else
+        echo "[$1] dead"
+    fi
+}
+
+###############################################################################
+###############################################################################
 
 case $ACTMETH in
+    restart)
+        $0 stop $2 $3 $4 $5
+        $0 start $2 $3 $4 $5
+        ;;
+
     start)
-        if [ -f $PIDFILE ]; then
-            PID=$(cat -- $PIDFILE)
-            STA=$(ps $PID)
-            if [ "$?" -eq "0" ] ; then
-                exit 0
-            fi
-            rm -f -- $PIDFILE
-        fi
-
-        if [ -f bin/activate ] ; then
-            source bin/activate
-        fi
-
-        exec ./manage.py runfcgi \
-            method=$SRVMETH protocol=$CGIPROT \
-            host=$HOSTVAL port=$PORTVAL \
-            pidfile=$PIDFILE daemonize=$DAEMOON
+        start $PIDFILE
         ;;
     stop)
-        if [ -f $PIDFILE ]; then
-            kill $(cat -- $PIDFILE) &> /dev/null
-            rm -f -- $PIDFILE
-        fi
-        ;;
-    restart)
-        $0 stop
-        $0 start
+        stop $PIDFILE
         ;;
     status)
-        if [ -f $PIDFILE ]; then
-            PID=$(cat -- $PIDFILE)
-            STA=$(ps $PID)
-            if [ "$?" -eq "0" ] ; then
-                echo "live: $PID"
-            else
-                echo "dead: $PID"
-            fi
-        else
-            echo "dead"
-        fi
+        status $PIDFILE
         ;;
+
+    stop-all)
+        for PIF in $(ls $PROJDIR/.pid-* 2> /dev/null) ; do
+            stop $PIF
+        done
+        ;;
+    status-all)
+        for PIF in $(ls $PROJDIR/.pid-* 2> /dev/null) ; do
+            status $PIF
+        done
+        ;;
+
     *)
         $0 restart
 esac
 
+###############################################################################
+###############################################################################
+
 exit 0
+
+###############################################################################
+###############################################################################
--- z-serve.sh
+++ z-serve.sh
@@ -7,23 +7,23 @@ EXECGRP="${USRNGRP/*:/}"
 
 case $ACTMETH in
     start)
-        sudo -u $EXECUSR -g $EXECGRP ./serve.sh start $3 $4 $5 $6 $7
+        sudo -u $EXECUSR -g $EXECGRP ./serve.sh start $3 $4 $5 $6
         ;;
     stop)
-        sudo -u $EXECUSR -g $EXECGRP ./serve.sh stop $3 $4 $5 $6 $7
+        sudo -u $EXECUSR -g $EXECGRP ./serve.sh stop $3 $4 $5 $6
         ;;
     restart)
-        sudo -u $EXECUSR -g $EXECGRP ./serve.sh restart $3 $4 $5 $6 $7
+        sudo -u $EXECUSR -g $EXECGRP ./serve.sh restart $3 $4 $5 $6
         ;;
     status)
-        sudo -u $EXECUSR -g $EXECGRP ./serve.sh status $3 $4 $5 $6 $7
+        sudo -u $EXECUSR -g $EXECGRP ./serve.sh status $3 $4 $5 $6
         ;;
     as)
         sudo chown $EXECUSR:$EXECGRP . -R
         ;;
     *)
         sudo chown $EXECUSR:$EXECGRP . -R
-        $0 restart
+        $0 restart $3 $4 $5 $6
 esac
 
 exit 0
