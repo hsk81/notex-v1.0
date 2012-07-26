d868938f0c01f3105ac1b9b1817f47ac4295846d
Hasan Karahan <hasan.karahan81@gmail.com>
2012-07-26 16:13:09 +0500

    Test for /editor/export-(report|pdf|latex|html)/

--- editor/tests.py
+++ editor/tests.py
@@ -18,111 +18,97 @@ import re
 ################################################################################
 
 class ModelTest (TestCase):
-
     def create_root (self, node_type=None):
-
         if node_type is None:
             node_type = ROOT_TYPE.objects.create (
-                code = self.create_id (),
-                desc = 'default root type')
+                code=self.create_id (),
+                desc='default root type')
 
         return ROOT.objects.create (
-            usid = self.create_id (),
-            name = 'root',
-            rank = 0,
-            type = node_type)
+            usid=self.create_id (),
+            name='root',
+            rank=0,
+            type=node_type)
 
     def test_root (self):
-
         root = self.create_root ()
         self.assertIsNotNone (root.id)
 
     def test_root_exception (self):
-
         node_type = BASE_TYPE.objects.create (
-            code = self.create_id (),
-            desc = 'invalid node type')
+            code=self.create_id (),
+            desc='invalid node type')
 
         self.assertRaises (FieldError, self.create_root, node_type)
 
     def create_node (self, node_type=None, node=None):
-
         if node_type is None:
             node_type = NODE_TYPE.objects.create (
-                code = self.create_id (),
-                desc = 'default node type')
+                code=self.create_id (),
+                desc='default node type')
 
         return NODE.objects.create (
-            root = self.create_root (),
-            node = node,
-            name = 'node',
-            rank = 0,
-            type = node_type)
+            root=self.create_root (),
+            node=node,
+            name='node',
+            rank=0,
+            type=node_type)
 
     def test_node (self):
-
         node = self.create_node ()
         self.assertIsNotNone (node.id)
         node = self.create_node (node=node)
         self.assertIsNotNone (node.id)
 
     def test_node_exception (self):
-
         base_type = BASE_TYPE.objects.create (
-            code = self.create_id (),
-            desc = 'invalid node type')
+            code=self.create_id (),
+            desc='invalid node type')
 
         node_type = NODE_TYPE.objects.create (
-            code = self.create_id (),
-            desc = 'default node type')
+            code=self.create_id (),
+            desc='default node type')
 
         node = self.create_node (node_type=node_type)
-        self.assertTrue(node.id != None)
+        self.assertTrue (node.id != None)
 
         self.assertRaises (FieldError, self.create_node, base_type)
         self.assertRaises (FieldError, self.create_node, base_type, node)
 
     def create_leaf (self, node_type=None):
-
         if node_type == None:
             node_type = LEAF_TYPE.objects.create (
-                code = self.create_id (),
-                desc = 'default leaf type')
+                code=self.create_id (),
+                desc='default leaf type')
 
         return LEAF.objects.create (
-            node = self.create_node (),
-            name = 'leaf',
-            rank = 0,
-            type = node_type)
+            node=self.create_node (),
+            name='leaf',
+            rank=0,
+            type=node_type)
 
     def test_leaf (self):
-
         leaf = self.create_leaf ()
         self.assertIsNotNone (leaf.id)
 
     def test_leaf_exception (self):
-
         node_type = BASE_TYPE.objects.create (
-            code = self.create_id (),
-            desc = 'invalid node type')
+            code=self.create_id (),
+            desc='invalid node type')
 
         self.assertRaises (FieldError, self.create_leaf, node_type)
 
-    def create_id (self, rmin = 0, rmax = 16**32-1):
-
+    def create_id (self, rmin=0, rmax=16 ** 32 - 1):
         return hex (random.randint (rmin, rmax)).lstrip ('0x').rstrip ('L')
 
 ################################################################################
 ################################################################################
 
 class ViewTest (TestCase):
-
     def setUp (self):
-
         self.test_main ()
 
     def tearDown (self):
-
         path_to_data = os.path.join (settings.MEDIA_DATA,
             self.client.session.session_key)
         if os.path.exists (path_to_data):
@@ -134,7 +120,7 @@ class ViewTest (TestCase):
             subprocess.check_call (['rm', path_to_temp, '-rf'])
 
         path_to_usid = os.path.join (settings.SESSION_FILE_PATH,
-                settings.SESSION_COOKIE_NAME + self.client.session.session_key)
+            settings.SESSION_COOKIE_NAME + self.client.session.session_key)
         if os.path.exists (path_to_usid):
             subprocess.check_call (['rm', path_to_usid, '-f'])
 
@@ -142,7 +128,6 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_main (self):
-
         resp = self.client.get ('/editor/?silent')
         self.assertEqual (resp.status_code, 200)
 
@@ -156,25 +141,23 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_create_project (self):
-
         resp = self.client.post ('/editor/create-project', {
             'nodeId': 'LMRHE33POQRCYIC3LVOQ====', ## root
             'data': json.dumps ({
-                'project':'PROJECT',
-                'authors':'AUTHORs',
-                'documentType':'article',
-                'fontSize':'12pt',
-                'columns':2,
-                'title':True,
-                'toc':True,
-                'index':False,
-                'content':'empty'}),
+                'project': 'PROJECT',
+                'authors': 'AUTHORs',
+                'documentType': 'article',
+                'fontSize': '12pt',
+                'columns': 2,
+                'title': True,
+                'toc': True,
+                'index': False,
+                'content': 'tutorial'}),
             'rank': '4'})
 
         return self.check (resp)
 
     def test_create_folder (self):
-
         resp, data = self.test_create_project ()
         resp = self.client.post ('/editor/create-folder', {
             'nodeId': data['id'],
@@ -184,7 +167,6 @@ class ViewTest (TestCase):
         return self.check (resp)
 
     def test_create_text (self):
-
         resp, data = self.test_create_project ()
         resp = self.client.post ('/editor/create-text', {
             'nodeId': data['id'],
@@ -195,7 +177,6 @@ class ViewTest (TestCase):
         return self.check (resp)
 
     def test_create_image (self):
-
         resp, data = self.test_create_project ()
         resp = self.client.post ('/editor/create-image', {
             'nodeId': data['id'],
@@ -206,7 +187,6 @@ class ViewTest (TestCase):
         return self.check (resp)
 
     def check (self, json_response):
-
         self.assertEqual (json_response.status_code, 200)
         self.assertIsNotNone (json_response.content)
         data = json.loads (json_response.content)[0]
@@ -219,7 +199,6 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_read_root (self):
-
         resp = self.client.post ('/editor/read/', {
             'node': 'LMRHE33POQRCYIC3LVOQ===='
         })
@@ -237,10 +216,9 @@ class ViewTest (TestCase):
         return  resp, data
 
     def test_read_node (self):
-
         resp, data = self.test_create_project ()
         resp = self.client.post ('/editor/read/', {'node': data['id']})
-        project_id =  data['id']
+        project_id = data['id']
 
         self.assertEqual (resp.status_code, 200)
         self.assertIsNotNone (resp.content)
@@ -257,7 +235,6 @@ class ViewTest (TestCase):
         return resp, data, project_id
 
     def test_read_leaf (self):
-
         resp, node_data, _ = self.test_read_node ()
         for el in node_data:
             resp = self.client.post ('/editor/read/', {'node': el['id']})
@@ -278,10 +255,8 @@ class ViewTest (TestCase):
         return self.check_update ('/editor/update-image')
 
     def check_update (self, url):
-
         resp, node_data, _ = self.test_read_node ()
         for el in node_data:
-
             resp = self.client.post (url, {
                 'leafId': el['id'],
                 'name': el['text'],
@@ -300,13 +275,12 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_increase_rank (self):
-        self.check_change_rank('/editor/increase-rank/')
+        self.check_change_rank ('/editor/increase-rank/')
 
     def test_decrease_rank (self):
-        self.check_change_rank('/editor/decrease-rank/')
+        self.check_change_rank ('/editor/decrease-rank/')
 
     def check_change_rank (self, url):
-
         resp, node_data, _ = self.test_read_node ()
         self.assertEquals (len (node_data), 2)
 
@@ -327,7 +301,6 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_rename_node (self):
-
         resp, node_data, project_id = self.test_read_node ()
         resp = self.client.post ('/editor/rename/', {
             'nodeId': project_id,
@@ -344,10 +317,8 @@ class ViewTest (TestCase):
         return resp, data
 
     def test_rename_leaf (self):
-
         resp, node_data, project_id = self.test_read_node ()
         for el in node_data:
-
             resp = self.client.post ('/editor/rename/', {
                 'nodeId': el['id'],
                 'name': 'new-name'
@@ -366,7 +337,6 @@ class ViewTest (TestCase):
     ###########################################################################
 
     def test_delete_node (self):
-
         resp, node_data, project_id = self.test_read_node ()
         resp = self.client.post ('/editor/delete/', {
             'id': project_id,
@@ -381,10 +351,8 @@ class ViewTest (TestCase):
         return resp, data
 
     def test_delete_leaf (self):
-
         resp, node_data, project_id = self.test_read_node ()
         for el in node_data:
-
             resp = self.client.post ('/editor/delete/', {
                 'id': el['id']
             })
@@ -396,6 +364,39 @@ class ViewTest (TestCase):
             self.assertEqual (data['id'], el['id'])
 
         return resp, node_data
+
+    ###########################################################################
+    ###########################################################################
+
+    def test_export_report (self):
+        self.check_export ('/editor/export-report/%s/', 'zip')
+
+    def test_export_pdf (self):
+        self.check_export ('/editor/export-pdf/%s/', 'pdf')
+
+    def test_export_latex (self):
+        self.check_export ('/editor/export-latex/%s/', 'zip')
+
+    def test_export_html (self):
+        self.check_export ('/editor/export-html/%s/', 'zip')
+
+    def check_export (self, url, ext):
+        resp, data = self.test_create_project ()
+        project_id = data['id']
+
+        resp = self.client.post (url % project_id)
+        self.check (resp)
+
+        resp = self.client.post (url % project_id)
+        self.assertEqual (resp.status_code, 200)
+        self.assertIsNotNone (resp.content)
+
+        self.assertEquals (resp['Content-Type'], 'application/%s' % ext)
+        self.assertTrue (resp['Content-Length'] > 0);
+        self.assertIsNotNone (re.match (
+            r'^attachment;filename="(.*).%s"$' % ext,
+            resp['Content-Disposition']
+        ));
 
 ################################################################################
 ################################################################################
