a04bda4f4d3e12b0254c64ce71ad58e05bff453d
Hasan Karahan <hasan.karahan81@gmail.com>
2012-08-14 18:53:09 +0500

    not required

--- setup.sh
+++ setup.sh
@@ -4,14 +4,11 @@ git submodule update --init
 virtualenv . --prompt="[notex] "
 source bin/activate
 
-pip install ipython
 pip install django
-pip install django-redis
 pip install django-jsmin
 pip install django-cssmin
 pip install psycopg2
 pip install pylibmc
-pip install numpy
 pip install pyyaml
 pip install flup
 
