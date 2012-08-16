0cd8ffe5582211d6fb323b13e985d0f4dd9df8a5
Hasan Karahan <hasan.karahan81@gmail.com>
2012-08-16 17:41:26 +0500

    minor

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ source bin/activate
 pip install django
 pip install django-jsmin
 pip install django-cssmin
-pip install psycopg2
+pip install psycopg2 ## may fail if no postgreqsql
 pip install pylibmc
 pip install pyyaml
 pip install flup
