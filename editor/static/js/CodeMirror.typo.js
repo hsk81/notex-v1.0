f3b903c97b7a1d36b9fc45acc095aaca6dc3e22b
Hasan Karahan <hasan.karahan81@gmail.com>
2012-09-14 18:25:45 +0500

    #102 Use spell checker only if English speaking, otherwise no default

--- editor/static/js/CodeMirror.typo.js
+++ editor/static/js/CodeMirror.typo.js
@@ -1,4 +1,31 @@
 window.onload = function () {
+
+    function get_lingua (language2country) {
+
+        function normalize (l2c, result) {
+            var l2c_list = l2c.replace ('-','_').split ('_');
+            return (l2c_list.length == 2)
+                ? l2c_list[0].toLowerCase () + '_' + l2c_list[1].toUpperCase ()
+                : result;
+        }
+
+        if (language2country &&
+            language2country.indexOf &&
+            language2country.indexOf ('en') !== -1) {
+            return normalize (language2country, 'en_US');
+        } else {
+            return undefined;
+        }
+    }
+
+    var lingua = get_lingua (
+        window.navigator.userLanguage || window.navigator.language
+    );
+
+    if (lingua == undefined) {
+        return; // do not set a default spell checker
+    }
+
     var worker = new Worker (
         location.static_url + 'app/editor/js/CodeMirror.typo.worker.js'
     );
@@ -12,7 +39,6 @@ window.onload = function () {
     Ext.ux.form.CodeMirror.typo_engine = null;
 
     worker.postMessage ({
-        lingua: 'en_US',
-        static: location.static_url
+        lingua: lingua, static: location.static_url
     });
 };
